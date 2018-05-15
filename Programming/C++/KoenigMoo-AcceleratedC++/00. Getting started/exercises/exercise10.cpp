ded3a8f9a8de91bd74332e12d9c6117838c8ce0d
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:22:10 -0500

    Treating std::cout as a value whose operations has side effects

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/exercises/exercise10.cpp
@@ -0,0 +1,10 @@
+// A small, introductory C++ program
+// COMPILE: make exercise10 && ./exercise10 && rm exercise10
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, World!";
+    std::cout << std::endl;
+    return 0;
+}
