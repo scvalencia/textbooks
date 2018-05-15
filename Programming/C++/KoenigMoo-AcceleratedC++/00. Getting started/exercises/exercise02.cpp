ee6d8061d4baf95ec13ad102e1bc7aa9d463dcec
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:20:40 -0500

    Exploring spacial characters while printing a simple string literal

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/exercises/exercise02.cpp
@@ -0,0 +1,8 @@
+// COMPILE: make exercise02 && ./exercise02 && rm exercise02
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "This (\") is a quote, and this (\\) is a backslash." << std::endl;
+    return 0;
+}
