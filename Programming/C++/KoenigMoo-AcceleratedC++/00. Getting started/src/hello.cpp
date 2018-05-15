020731839ffaa9edb53befae9d67842a3a8047fd
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:19:46 -0500

    A simple, yet very useful small C++ program

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/src/hello.cpp
@@ -0,0 +1,9 @@
+// A small, introductory C++ program
+// COMPILE: make hello && ./hello && rm hello
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
