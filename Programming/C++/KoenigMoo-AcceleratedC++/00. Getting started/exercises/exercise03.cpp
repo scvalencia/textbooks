f239e48862cabe796628f2c0e73ceeca7a18dd06
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:20:46 -0500

    Exploring spacial characters while printing a simple string literal

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/exercises/exercise03.cpp
@@ -0,0 +1,8 @@
+// COMPILE: make exercise03 && ./exercise03 && rm exercise03
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello\tBrian\tAdolf\tKernighan." << std::endl;
+    return 0;
+}
