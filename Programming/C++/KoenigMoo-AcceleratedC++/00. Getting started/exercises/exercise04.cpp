fee16a312024231298cc9f1ccd129b9b32227b47
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:21:23 -0500

    A Quine? in the C++ programming language. Well, I'm just printing the content of another program in this one

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/exercises/exercise04.cpp
@@ -0,0 +1,23 @@
+// COMPILE: make exercise04 && ./exercise04 && rm exercise04
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    std::cout << "// A small, introductory C++ program" << std::endl;
+    std::cout << std::endl << std::endl;
+    std::cout << "#include <iostream>" << std::endl;
+    std::cout << std::endl << std::endl;
+    std::cout << "int main(int argc, char const *argv[]) {" << std::endl;
+    std::cout << "\tstd::cout << \"Hello, World!\" << std::endl;" << std::endl;
+    std::cout << "\treturn 0;" << std::endl;
+    std::cout << "}" << std::endl;
+
+
+    return 0;
+}
+
+
+
+
+
