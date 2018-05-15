e0eea338d7c516a86672858c5bac71ca9665be8f
scvalencia <sc.valencia606@uniandes.edu.co>
2018-05-14 21:20:08 -0500

    Compiling the hello world program

---
+++ Programming/C++/KoenigMoo-AcceleratedC++/00. Getting started/exercises/exercise00.cpp
@@ -0,0 +1,9 @@
+// A small, introductory C++ program
+// COMPILE: make exercise00 && ./exercise00 && rm exercise00
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
